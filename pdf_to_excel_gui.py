d7223bf91d506b1a554906460d98d40ae923d2a3
helloboraS <ksbr1004@nate.com>
2025-05-23 11:21:13 +0900

    Add files via upload

--- pdf_to_excel_gui.py
+++ pdf_to_excel_gui.py
@@ -12,6 +12,7 @@ def extract_format_a(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             lines = page.extract_text().split("\n")
+
             
             for line in lines:
                 parts = line.split()
@@ -51,7 +52,8 @@ def extract_format_b(pdf_path):
     records = []
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            lines = page.extract_text().split("\n")  # ✅ 올바르게 줄바꿈 적용
+            lines = page.extract_text().split("\n")
+  # ✅ 올바르게 줄바꿈 적용
             
             i = 0
             while i < len(lines) - 2:
