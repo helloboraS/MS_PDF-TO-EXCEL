0b635aa11531ba06967e30da15c77991852ffe0e
helloboraS <ksbr1004@nate.com>
2025-07-03 22:36:30 +0900

    Add files via upload

--- pdf_to_excel_gui.py
+++ pdf_to_excel_gui.py
@@ -160,7 +160,7 @@ with tab2:
 
 with tab3:
 
-    
+
     # st.header("📒 마스터 데이터 비교")
 
     if "master_df" not in st.session_state:
@@ -175,7 +175,7 @@ with tab3:
 
     def clean_code(code):
         return str(code).strip().replace("-", "")
-        
+
     def fix_hscode(code):
         try:
             code_str = str(code)
@@ -189,13 +189,13 @@ with tab3:
         input_df = pd.read_excel(uploaded_excel)
         master_df = master_df.rename(columns=lambda x: x.strip())
         input_df = input_df.rename(columns=lambda x: x.strip())
-        
+
         input_df["Microsoft Part No."] = input_df["Microsoft Part No."].astype(str).str.strip()
         master_df["Microsoft Part No."] = master_df["Microsoft Part No."].astype(str).str.strip()
-        
+
         merged = input_df.merge(master_df, how="left", on="Microsoft Part No.")
         merged["INV HS"] = merged["INV HS"].apply(clean_code)
-        
+
 
 
         merged["HS Code"] = merged["HS Code"].apply(clean_code).apply(fix_hscode)
@@ -245,7 +245,7 @@ with tab3:
                 data=f,
                 file_name="MS5673_신고.xlsx"
             )
-    
+
     elif master_df is not None:
         st.markdown("---")
         #st.subheader("🔍 단일 Microsoft Part No. 수기 비교")
@@ -398,7 +398,7 @@ with tab4:
             final["Microsoft Part No."] = final["Microsoft Part No."].fillna("신규코드")
             final["Part Description"] = final["Part Description"].fillna(final["Description"])
 
-            
+
     final["matched_by_item"] = final["clean_item"] == final["clean_code"]
     final["matched_by_desc"] = final["clean_desc"] == final["clean_desc"]
 
