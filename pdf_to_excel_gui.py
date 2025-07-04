0f34b486845362046ad9f826659d4fa39d944b87
helloboraS <ksbr1004@nate.com>
2025-07-04 16:29:34 +0900

    Add files via upload

--- pdf_to_excel_gui.py
+++ pdf_to_excel_gui.py
@@ -427,33 +427,12 @@ with tab4:
                         origin_map[item] = origin_val
 
             
-            # Export Code 추출
-            export_code_map = {}
-            for idx, line in enumerate(lines_by_page):
-                for item in item_list:
-                    if item.strip() in line:
-                        export_val = "미확인"
-                        for next_line in lines_by_page[idx:]:
-                            merged_line = next_line.strip()
-                            if idx + 1 < len(lines_by_page):
-                                merged_line += " " + lines_by_page[idx + 1].strip()
-                            if idx + 2 < len(lines_by_page):
-                                merged_line += " " + lines_by_page[idx + 2].strip()
-                            
-                            
-                            
-                            match_export = re.search(r"Export\s*Code\s*[:：]?\s*([\d\.\-]+)", merged_line, re.IGNORECASE)
-                            match_hs = re.search(r"HS\s*Code\s*[:：]?\s*([\d\.\-]+)", merged_line, re.IGNORECASE)
-                            if match_export:
-                                export_val = match_export.group(1)
-                                break
-                            elif match_hs:
-                                export_val = match_hs.group(1)
-                                break
-                        export_code_map[item] = export_val
-
-            final["Export Code"] = final["Item Number"].map(export_code_map).fillna("미확인")
-
+            
+def extract_export_code_map(lines_by_page, item_list):
+    """
+    각 Item 기준으로 Export Code or HS Code 값을 찾아주는 함수
+    """
+    
 # origin_map을 final에 적용
             final["Country of Origin"] = final["Item Number"].map(origin_map).fillna("미확인")
 
